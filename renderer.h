2f6646feea02cd48e309c557c4f7190e88d6e268
jacob lindborg <65565795+s5msepi0l@users.noreply.github.com>
2024-03-06 21:55:28 +0200

    Update renderer.h

--- renderer.h
+++ renderer.h
@@ -2,10 +2,10 @@
 #define PLATFORMER_RENDERER
 
 #include "util.h"
+#include "sprite.h"
 
 #include <vector>
 #include <ctime>
-#include <SDL2/SDL.h>
 
 #define VISABLE   1
 #define INVISIBLE 0
@@ -15,14 +15,6 @@ typedef struct {
     u8 g = 255;
     u8 b = 255;
 }rgb;
-
-
-// pixel is invisible unless specifically specified
-typedef struct rgb_sprite {
-    rgb clr;
-    u8 state = 0;
-} rgb_sprite;
-
 
 // mainly used for storing pixel offset but can be used for most other applications as long as the 
 // required prescision > 65k
@@ -36,37 +28,7 @@ typedef struct {
  * I'm hoping that designing each element to only have a rgb value won't bite me in the ass later
  * automatically allocated to heap
 */
-template <typename T>
-class buffer_2D {
-private:
-    std::vector<T> data;
 
-public:
-    int width;
-    int height;
-
-    //preallocate memory to it (x * y * sizeof(rgb))
-    buffer_2D(int w, int h):
-    data(w * h),
-    width(w),
-    height(h)
-    {    
-    }
-
-    // get direct data pointer
-    std::vector<T> ref() const { return this->data; }
-
-    T *operator[](int index) {
-        std::cout << "index:" << index << '\n';
-        return &this->data[index * width];
-    }
-
-    const T *operator[](int index) const {
-        std::cout << "index:" << index << '\n';
-        return &data[index * width];
-    }
-
-};
 
 class pipeline_renderer {
 private:
@@ -117,6 +79,9 @@ public:
             SDL_Log("SDL renderer could not be initialized! SDL_Error: %s\n", SDL_GetError());   
         }
 
+
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderClear(renderer);
         SDL_RenderPresent(renderer);
@@ -162,30 +127,26 @@ public:
     void pipeline_input(buffer_2D<rgb_sprite> &obj, f_vec_2D offset) {
         for (i32 i = 0; i<obj.width; i++) {
             for (i32 j = 0; j<obj.height; j++) {
-                if (obj[i][j].state == VISABLE) {
-                    std::cout << "DRAWING\n";
-                    SDL_Rect rect;
-                    std::cout << "offset x: " << (offset.x * tile_scale) << '\n';
-                    std::cout << "offset y: " << (offset.y * tile_scale) << '\n';
-                    rect.x = (i * tile_scale) + (offset.x * tile_scale);
-                    rect.y = (j * tile_scale) + (offset.y * tile_scale);
-                    rect.w = tile_scale;
-                    rect.h = tile_scale;
 
-                    rgb clr = obj[i][j].clr;
+                SDL_Rect rect;  
+                rect.x = (i * tile_scale) + (offset.x * tile_scale);
+                rect.y = (j * tile_scale) + (offset.y * tile_scale);
+                rect.w = tile_scale;
+                rect.h = tile_scale;
 
-                    SDL_SetRenderDrawColor(
-                        renderer,
-                        clr.r,
-                        clr.g,
-                        clr.b,
-                        255
-                    );
+                rgb_sprite clr = obj[i][j];
+                SDL_SetRenderDrawColor(
+                    renderer,
+                    clr.r,
+                    clr.g,
+                    clr.b,
+                    clr.a
+                );
 
-                    SDL_RenderFillRect(renderer, &rect);
-                }
+                SDL_RenderFillRect(renderer, &rect);
             }
         }
+        
     }
 
     void pipeline_input(buffer_2D<rgb> obj) {
