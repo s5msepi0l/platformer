a4d8bd6a8ce77d2e3940ab7555b6c18567d819df
jacob lindborg <65565795+s5msepi0l@users.noreply.github.com>
2024-03-06 21:54:32 +0200

    Update util.h

--- util.h
+++ util.h
@@ -6,12 +6,16 @@
 #include <fstream>
 #include <cmath>
 #include <map>
+#include <list>
+#include <unordered_map>
+#include <SDL2/SDL.h>
 
 namespace std {
     //generally used for holding ascii values or 
     typedef basic_string<unsigned char> ustring;
 }
 
+//compiler doesn't really like when i try to use a u8 as a boolean so im just gonna use a regular bool 
 #define f32 float
 #define f64 double 
 
@@ -41,7 +45,116 @@ typedef struct f_vec_2D {
     inline f32 distance_to(f32 x_pos, f32 y_pos) const {
         return static_cast<f32>(sqrt(pow(x_pos, 2) + pow(y_pos, 2)));
     }
+
+    void operator+=(f_vec_2D other) {
+        x += other.x;
+        y += other.y;
+    }
+
+    inline f_vec_2D operator*(f32 op) {
+        return f_vec_2D(this->x * op, this->y * op);
+    }
+
 }f_vec_2D;
+
+class keyboard_input {
+public:
+    inline f_vec_2D poll() {
+        // Get the state of all keys
+        const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
+        f_vec_2D ret;
+
+        // Check if the keys are pressed using SDL_GetKeyState
+        if (currentKeyStates[SDL_SCANCODE_UP]) {
+            ret.y -= 1.0;
+        }
+        if (currentKeyStates[SDL_SCANCODE_DOWN]) {
+            ret.y += 1.0;
+        }
+        if (currentKeyStates[SDL_SCANCODE_LEFT]) {
+            ret.x -= 1.0;
+        }
+        if (currentKeyStates[SDL_SCANCODE_RIGHT]) {
+            ret.x += 1.0;
+        }
+
+        return ret;
+    }
+};
+
+template <typename T>
+class buffer_2D {
+private:
+    std::vector<T> data;
+
+public:
+    int width = 0;
+    int height = 0;
+
+    buffer_2D() {}
+
+    //preallocate memory to it (x * y * sizeof(rgb))
+    buffer_2D(int w, int h):
+    data(w * h),
+    width(w),
+    height(h)
+    {    
+    }
+
+    // get direct data pointer
+    std::vector<T> ref() const { return this->data; }
+    inline bool is_empty() const { return data.empty(); }
+
+    T *operator[](int index) {
+        return &this->data[index * width];
+    }
+
+    const T *operator[](int index) const {
+        return &data[index * width];
+    }
+
+};
+
+template <typename key_type, typename value_type>
+class lru_cache {
+private:
+    int capacity;
+    std::list<std::pair<key_type, value_type>> cache_list; // Represents the cache as a doubly linked list of key-value pairs
+    std::unordered_map<key_type, typename std::list<std::pair<key_type, value_type>>::iterator> cache_map; // Maps keys to iterators in the list
+
+public:
+    lru_cache(int capacity) : capacity(capacity) {}
+
+    value_type get(const key_type& key) {
+        auto it = cache_map.find(key);
+
+        if (it == cache_map.end()) {
+            return value_type(); 
+        }
+
+        cache_list.splice(cache_list.begin(), cache_list, it->second);
+
+        return it->second->second;
+    }
+
+    void put(const key_type& key, const value_type& value) {
+        auto it = cache_map.find(key);
+        
+        if (it != cache_map.end()) {
+            it->second->second = value;
+            cache_list.splice(cache_list.begin(), cache_list, it->second);
+        } else {
+            if (cache_list.size() == capacity) {
+                key_type old_key = cache_list.back().first;
+                cache_map.erase(old_key);
+                cache_list.pop_back();
+            }
+
+            cache_list.emplace_front(key, value);
+            cache_map[key] = cache_list.begin();
+        }
+    }
+};
 
 //general ascii file reader 
 std::string f_read(std::string path) {
