fe8539baf6eac5608718cb7b8cae074e20210419
jacob lindborg <65565795+s5msepi0l@users.noreply.github.com>
2024-03-06 21:54:55 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include "game.h"
 
 int main(int argc, const char *argv[]) {
-    pipeline_renderer renderer(1280, 720, "game", 2);
-
-    for (i32 i = 0; i<1000 * 1000; i++) {
-        renderer.test();
-        SDL_Delay(125);
-    }
+    game platformer("Game", 1280, 720, 30);
+    platformer.run();
 
     return 0;
 }
